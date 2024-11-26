6a8e1885c2d5cc1586f89634cfe0b0f95d61263a
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-11-26 13:42:40 -0800

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "images.unsplash.com",
+        },
+        {
+          protocol: "https",
+          hostname: "imagedelivery.net",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
---
+++ src/app/product/[productId]/page.tsx
@@ -0,0 +1,71 @@
+import ProductGallery from "@/app/product/[productId]/ProductGallery";
+import ProductInfo from "@/app/product/[productId]/ProductInfo";
+
+export const runtime = 'edge';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ productId: number }>;
+}) {
+    await new Promise((resolve) => setTimeout(resolve, 1500)); // 3-second delay
+
+  const productId = (await params).productId;
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
+          <ProductGallery
+            mainImage={products[productId-1].image}
+            // thumbnails={productData.thumbnails}
+          />
+          <div>
+            <ProductInfo
+              title="Product"
+              price="$100"
+              discount="Svae 2323r2"
+              rating={1}
+              reviews={23}
+              description="Cool product"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const products = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 1059.99,
+      category: "Technology",
+      image:
+        "https://imagedelivery.net/llMDWXFPgX44M9elMfQ9XA/393defea-8ee6-46c7-971f-df55f24a7700/public",
+    },
+    {
+      id: 2,
+      name: "Television",
+      price: 2889.99,
+      category: "Technology",
+      image:
+        "https://imagedelivery.net/llMDWXFPgX44M9elMfQ9XA/f2cf1abd-3995-4b8c-0c24-99b82e4aac00/public",
+    },
+    {
+      id: 3,
+      name: "Keyboard",
+      price: 129.99,
+      category: "Accessories",
+      image:
+        "https://imagedelivery.net/llMDWXFPgX44M9elMfQ9XA/4e290265-b01d-43d9-0b49-b891fcdaf100/public",
+    },
+    {
+      id: 4,
+      name: "Watch",
+      price: 199.99,
+      category: "Accessories",
+      image:
+        "https://imagedelivery.net/llMDWXFPgX44M9elMfQ9XA/36b74004-c669-4572-b505-6384c000bd00/public",
+    },
+  ];
